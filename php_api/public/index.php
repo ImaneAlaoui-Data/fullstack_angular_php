553aab528b99b1ac986eeb6f8a965528963404ee
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2024-05-27 13:29:12 +0200

    Refactor index as simple dispatcher

--- php_api/public/index.php
+++ php_api/public/index.php
@@ -4,7 +4,7 @@
  * @author Aélion <jean-luc.aubert@aelion.fr>
  * @version 1.0.0
  *  - Instanciate Kernel
- */
+
 require_once('./../vendor/autoload.php');
 
 use Aelion\Kernel;
@@ -15,3 +15,5 @@ error_reporting(E_ALL & ~E_NOTICE);
 $kernel = Kernel::create();
 $response = $kernel->processRequest();
 echo $response->send();
+*/
+echo 'Hello PHP';
